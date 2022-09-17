45e2cda92f5ae1cc11d3bae3342c229a7bc46eb8
denyys <107223125+denyysdenyy@users.noreply.github.com>
2022-09-17 13:33:18 +0300

    Delete shopify directory

--- shopify/js/main.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.header__menu');
-const menuBtn = document.querySelector('.header__btn')
-
-menuBtn.addEventListener ('click', () => {
-    menu.classList.toggle ('active');
-    menuBtn.classList.toggle ('active');
-})
